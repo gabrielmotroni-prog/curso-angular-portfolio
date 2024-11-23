c2c108da0f7cccc7f683f93a864a401f490509a5
Gabriel Motroni <gabriel.motroni@hotmail.com>
2024-11-23 12:27:41 -0300

    feat co html componente projects

--- src/app/modules/portfolio/pages/home.component.ts
+++ src/app/modules/portfolio/pages/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { KnownledgeComponent } from '../components/knownledge/knownledge.component';
 import { ExperiencesComponent } from '../components/experiences/experiences.component';
+import { ProjectsComponent } from '../components/projects/projects.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, KnownledgeComponent,ExperiencesComponent],
+  imports: [HeaderComponent, KnownledgeComponent,ExperiencesComponent,ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
