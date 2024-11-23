85c2a540b8e494cc5b4189f89085bf8b08752212
Gabriel Motroni <gabriel.motroni@hotmail.com>
2024-11-23 10:44:18 -0300

    feat componente knowledge html

--- src/app/modules/portfolio/pages/home.component.ts
+++ src/app/modules/portfolio/pages/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
+import { KnownledgeComponent } from '../components/knownledge/knownledge.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, KnownledgeComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
