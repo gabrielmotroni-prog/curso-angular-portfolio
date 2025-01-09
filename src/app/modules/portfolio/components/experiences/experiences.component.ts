44fa81ab9a1e21a678a4538d64b8f8f35e71d46d
C1325300 Gabriel Dos Santos Motroni <c1325300@interno.bb.com.br>
2025-01-09 14:23:29 -0300

    feature atualizada dados

--- src/app/modules/portfolio/components/experiences/experiences.component.ts
+++ src/app/modules/portfolio/components/experiences/experiences.component.ts
@@ -12,24 +12,24 @@ export class ExperiencesComponent {
   public arrayExperiences = signal<IExperiences[]>([
     {
       summary: {
-        strong: 'Front-end Especilista',
-        p: 'ZUP | set 2020 - Present',
+        strong: 'Analista de Software Sênior',
+        p: 'Qintess | Dez 2024 - Atual',
       },
-      text: '<p> Desde setembro de 2020, atuo como analista de sistema especilista na zup, nas areas</p> ',
+      text: '<p> Desde Dezembro de 2024, atuo como análista de sistema sênior fullstack na Qintess, atuando em consultoria no cliente banco do brasil</p> ',
     },
     {
       summary: {
-        strong: 'Fullstack Developer',
-        p: 'Everis | out 2019 - set 2020',
+        strong: 'Application Developer',
+        p: 'IBM | Out 2022 - Nov 2024',
       },
-      text: '<p> Durante meu periodo na everis de o </p> ',
+      text: '<p> Atuei como desenvolvedor junior e apos um ano fui reconhecido com um promocação na empresa. Atuei como desenvolvedor fullstack em consultoria  </p> ',
     },
     {
       summary: {
-        strong: 'Consultor Web',
-        p: 'Everis | out 2019 - set 2020',
+        strong: 'Application Developer Intern',
+        p: 'IBM | Nov 2021 - Out 2022',
       },
-      text: '<p>  Em minha jornada como consultor web</p>',
+      text: '<p> Durante meu periodo estágio na IBM </p> ',
     },
   ]);
 }
